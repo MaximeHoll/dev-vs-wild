14a99d6290fde92a520be9157a4fc687a2c6c555
Colin <colin25.huart@hotmail.com>
2023-03-25 10:37:48 +0100

    route timder

--- server/models/index.js
+++ server/models/index.js
@@ -1,23 +1,19 @@
-const sequelize = require('../config/sequelizeDb')
+const sequelize = require("../config/sequelizeDb");
 const { Sequelize, DataTypes } = require("sequelize");
 
 //filling db variable with models with sequelize+Datatypes to use them elsewhere
 
-const db = {}
+const db = {};
 
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-db.users=require('./User')(sequelize, DataTypes)
+db.users = require("./User")(sequelize, DataTypes);
+db.arbres_tinder = require("./Timder")(sequelize, DataTypes);
 
 //Relationships between models
-
-
-
-
 
 //With Sequelize sync should be done with force:false to avoid tables dropping at every connection. Gives a bug here (probably because Models and db have a slight variation) but the db works fine still.
 //Code can be left commented.
 
-
-module.exports = db
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,43 +1,44 @@
 const cors = require("cors");
 const express = require("express");
 require("dotenv").config({ path: "./config/.env" });
-const sequelize = require('./config/sequelizeDb')
-const db = require('./models/index')
-
+const sequelize = require("./config/sequelizeDb");
+const db = require("./models/index");
 
 //connection to db
 
-sequelize.authenticate().then(() => {
-    
-    console.log('Connection has been established successfully.');
- }).catch((error) => {
-    console.error('Unable to connect to the database: ', error);
- });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
 
-db.sequelize.sync({force: false})
-.then(() => {
-    console.log('Resync done')
-})
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("Resync done");
+});
 
 // running app
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
-app.use(express.json())
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
 
 //routes variables
 
-const authRoutes = require('./routes/auth.routes')
-
+const authRoutes = require("./routes/auth.routes");
+const timbRoutes = require("./routes/timder.routes");
 //routes
 
-app.use('/api/auth', authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/timder", timbRoutes);
 
 //app listen
 
 app.listen(process.env.PORT, () => {
-    console.log(`App server now listening on port ${process.env.PORT}`);
+  console.log(`App server now listening on port ${process.env.PORT}`);
 });
 
 module.exports = app;
