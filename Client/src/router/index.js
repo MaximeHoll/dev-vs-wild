ef691801022765822812388423c64739bc1d1ce0
Colin <colin25.huart@hotmail.com>
2023-03-25 19:10:36 +0100

    merge

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/TimbderVue.vue";
-import AboutView from "../views/AboutView.vue";
-=======
-import { createRouter, createWebHistory } from 'vue-router'
-import TimbderVue from '../views/TimbderVue.vue'
-import HomeView from '../views/HomeView.vue'
->>>>>>> main
+import TimbderVue from "../views/TimbderVue.vue";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
