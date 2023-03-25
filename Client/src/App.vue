6a2b9c216ff5e3e22f620f01c2aa2cb91c83e15c
Elhya13 <aureliewinkin@gmail.com>
2023-03-25 11:12:05 +0100

    reset home/order tree

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <!-- <RouterLink to="/about">About</RouterLink> -->
       </nav>
     </div>
   </header>
