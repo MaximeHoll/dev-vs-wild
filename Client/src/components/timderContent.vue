6e37c902eadfcfdebe001356c967def34db51d98
LukaB15 <lukabrabant15@gmail.com>
2023-03-25 21:35:24 +0100

    carousel fix

--- Client/src/components/timderContent.vue
+++ Client/src/components/timderContent.vue
@@ -4,10 +4,10 @@
 
             <h3 class="text-white font-Outfit font-semibold text-2xl">{{ slide.text }}</h3>
 
-            <RouterLink :to="{ name: 'TimbderVue'}" class="flex justify-center">
-            <button class="bg-greyh pt-4 pb-4 pl-12 pr-12 rounded-3xl">
-                  <img :src="`${slide.button}`" class="w-16 -z-0"/>
-            </button>
+            <RouterLink :to="{ name: 'timbder'}" class="flex justify-center">
+                  <button class="bg-greyh pt-4 pb-4 pl-12 pr-12 rounded-3xl">
+                        <img :src="`${slide.button}`" class="w-16 -z-0"/>
+                  </button>
             </RouterLink>
 
             <h3 class="text-white font-Outfit font-semibold text-2xl pt-8">{{ slide.text2 }}</h3>
--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     // {
     //   path: '/about',
