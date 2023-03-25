644f7b2117675454d3b0bb323791aeb3e1408d66
LukaB15 <lukabrabant15@gmail.com>
2023-03-25 12:34:52 +0100

    oui

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -5,17 +5,10 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <!-- <RouterLink to="/about">About</RouterLink> -->
-      </nav>
-    </div>
+    <p class="bg-black">oui</p>
   </header>
+
+  <RouterLink to="/">Home</RouterLink>
 
   <RouterView />
 </template>
